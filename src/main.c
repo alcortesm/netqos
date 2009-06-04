db6d7e2e591aa3cdc0ae14bd0ebeb310e05ecd65
Alberto Cortés <alcortes@it.uc3m.es>
2009-06-05 00:49:41 +0200

    Add debug traces to module init and exit

--- src/main.c
+++ src/main.c
@@ -6,10 +6,14 @@ MODULE_AUTHOR("Alberto Cortes <alcortes@it.uc3m.es>");
 MODULE_VERSION("0.1");
 
 static int __init netqos_init(void) {
+    printk(KERN_DEBUG "netqos_init(): called\n");
+    printk(KERN_DEBUG "netqos_init(): return 0 (success)\n");
     return 0; /* success */
 }
 
 static void __exit netqos_exit(void) {
+    printk(KERN_DEBUG "netqos_exit(): called\n");
+    printk(KERN_DEBUG "netqos_exit(): returns\n");
     return;
 }
 
