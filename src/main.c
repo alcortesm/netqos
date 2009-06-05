4ff2b964b3fedb7cf45699b6ae8462d8ae6180c3
Alberto Cortés <alcortes@it.uc3m.es>
2009-06-05 14:43:38 +0200

    Add global show and store for attributes
    Also add netqos.h and some debug and info messages based on fuse

--- src/main.c
+++ src/main.c
@@ -4,27 +4,30 @@
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
 
+#include "netqos.h"
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Alberto Cortes <alcortes@it.uc3m.es>");
-MODULE_VERSION("0.2");
+MODULE_VERSION(NETQOS_VERSION);
 
 /* declaration of attribute access functions */
-static ssize_t foo_show(struct kobject *kobj,
+/* show() and store() are demultiplexers for the rest
+ * using attr as the selector */
+static ssize_t show(struct kobject *kobj,
         struct kobj_attribute *attr, char *buf);
-static ssize_t foo_store(struct kobject *kobj,
+static ssize_t store(struct kobject *kobj,
         struct kobj_attribute *attr, const char *buf,
         size_t count);
-static ssize_t bar_show(struct kobject *kobj,
-        struct kobj_attribute *attr, char *buf);
-static ssize_t bar_store(struct kobject *kobj,
-        struct kobj_attribute *attr, const char *buf,
-        size_t count);
+static ssize_t foo_show(char *buf);
+static ssize_t foo_store(const char *buf, size_t count);
+static ssize_t bar_show(char *buf);
+static ssize_t bar_store(const char *buf, size_t count);
 
 /* attributes */
 static struct kobj_attribute foo_attribute =
-    __ATTR(foo, 0666, foo_show, foo_store);
+    __ATTR(foo, 0666, show, store);
 static struct kobj_attribute bar_attribute =
-    __ATTR(bar, 0666, bar_show, bar_store);
+    __ATTR(bar, 0666, show, store);
 static struct attribute *attrs[] = {
     &foo_attribute.attr,
     &bar_attribute.attr,
@@ -37,30 +40,53 @@ static int bar;
 
 /* definitions of attribute accesss functions */
 static ssize_t
-foo_show(struct kobject *kobj, struct kobj_attribute *attr,
+show(struct kobject *kobj, struct kobj_attribute *attr,
         char *buf)
+{
+    if (attr == &foo_attribute) {
+        return foo_show(buf);
+    }
+    else if (attr == &bar_attribute) {
+        return bar_show(buf);
+    } else {
+        return -1;
+    }
+}
+
+static ssize_t
+store(struct kobject *kobj, struct kobj_attribute * attr,
+        const char *buf, size_t count)
+{
+    if (attr == &foo_attribute) {
+        return foo_store(buf, count);
+    } else if (attr == &bar_attribute) {
+        return bar_store(buf, count);
+    } else {
+        return -1;
+    }
+}
+
+static ssize_t
+foo_show(char *buf)
 {
     return sprintf(buf, "%d\n", foo);
 }
 
 static ssize_t
-foo_store(struct kobject *kobj, struct kobj_attribute * attr,
-        const char *buf, size_t count)
+foo_store(const char *buf, size_t count)
 {
     sscanf(buf, "%du", &foo);
     return count;
 }
 
 static ssize_t
-bar_show(struct kobject *kobj, struct kobj_attribute *attr,
-        char *buf)
+bar_show(char *buf)
 {
     return sprintf(buf, "%d\n", bar);
 }
 
 static ssize_t
-bar_store(struct kobject *kobj, struct kobj_attribute * attr,
-        const char *buf, size_t count)
+bar_store(const char *buf, size_t count)
 {
     sscanf(buf, "%du", &bar);
     return count;
@@ -80,6 +106,9 @@ static int __init
 netqos_init(void)
 {
     int r;
+
+    printk(KERN_INFO "netqos init (API version %i)\n",
+            NETQOS_VERSION);
 
     /* Create a simple kobject called "netqos" under /sys/kernel
      * 
@@ -101,6 +130,8 @@ netqos_init(void)
 static void __exit
 netqos_exit(void)
 {
+    printk(KERN_DEBUG "netqos exit\n");
+
     kobject_put(netqos_kobj);
     return;
 }
---
+++ src/netqos.h
@@ -0,0 +1,6 @@
+#ifndef _NETQOS_H
+#define _NETQOS_H
+
+#define NETQOS_VERSION "0.2"
+
+#endif /* _NETQOS_H */
